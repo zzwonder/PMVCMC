663c454323bc26e93a200ad164ba51391374c845
Zhiwei Zhang <zhiwei@rice.edu>
2022-10-05 22:49:43 -0500

    start writing NEPM

--- main.py
+++ main.py
@@ -49,6 +49,22 @@ def getEdgeString(e):
     return "edge " + repr(e[0]) + " and " + repr(e[1]) + " with color " + repr(e[2]) + " and " + repr(e[3])
 
 
+def getTutteVariableString(v):
+    return "vertex %d is in Tutte Set" % v
+
+
+def getRestEdgeString(e):
+    return "edge %d %d %d %d is in the subgraph of V-S" % (e[0], e[1], e[2], e[3])
+
+
+def getConnectedComponentString(v,i):
+    return "vertex %d is in component %i" % (v,i)
+
+
+def getOddComponentString(i):
+    return "component %d is an odd component" % i
+
+
 def generatePMFormula(graph, formulaPath):
     varMap = {}
     for e in graph.edges:
@@ -89,9 +105,56 @@ def generatePMFormula(graph, formulaPath):
         f.write('\n')
 
 
+
 def generateNEPMFormula(graph, formulaPath):
-    with open(formulaPath) as f:
-        pass
+    varMap = {}
+    for i in range(1, graph.n + 1):
+        allocateVar(varMap,getTutteVariableString(i))
+        allocateVar(varMap, getOddComponentString(i))
+        for j in range(1,graph.n + 1):
+            allocateVar(varMap, getConnectedComponentString(i,j))
+    for e in graph.edges:
+        allocateVar(varMap, getEdgeString(e))
+        allocateVar(varMap, getRestEdgeString(e))
+    for i in range(1, graph.n + 1):
+        for j in range(1, graph.d + 1):
+            allocateVar(varMap, getVCString(i, j))
+    print(varMap)
+    with open(formulaPath, 'w+') as f:
+        # exact-one for ad-hoc color of each vertex
+        f.write("c exact-one for ad-hoc color of each vertex\n")
+        for i in range(1, graph.n + 1):
+            f.write("eo ")
+            for j in range(1, graph.d + 1):
+                f.write(repr(varMap[getVCString(i, j)]) + " ")
+            f.write('\n')
+        for e in graph.edges:
+            f.write("%d <-> ( ! %d & ! %d ) & ( %d & %d ) \n" % (varMap[getRestEdgeString(e)], varMap[getTutteVariableString(e[0])], varMap[getTutteVariableString(e[1])], varMap[getVCString(e[0], e[2])],
+                                                               varMap[getVCString(e[1],e[3])]))
+        for e in graph.edges:
+            for i in range(1,graph.n+1):
+                f.write("%d -> ( %d = %d )\n" % (varMap[getRestEdgeString(e)], varMap[getConnectedComponentString(e[0],i)], varMap[getConnectedComponentString(e[1],i)] ))
+
+        for i in range(1, graph.n+1):
+            f.write("x %d " % varMap[getOddComponentString(i)])
+            for j in range(1, graph.n  + 1 ):
+                f.write("%d " % varMap[getConnectedComponentString(j,i)])
+            f.write("\n")
+
+        for i in range(1, graph.n+1):
+            f.write('eo ')
+            for j in range(1, graph.n  + 1 ):
+                f.write("%d " % varMap[getConnectedComponentString(i,j)])
+            f.write("\n")
+
+        f.write('card ')
+        for i in range(1, graph.n+1):
+            f.write("%d - %d " % (varMap[getOddComponentString(i)], varMap[getTutteVariableString(i)]))
+        f.write("> 0 ;\n")
+
+
+
+
 
 
 def PBEncoding(formulaPath, PBPath):
@@ -118,8 +181,9 @@ def PBEncoding(formulaPath, PBPath):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     graph = Graph()
-    #graph.readGraph('graph.txt')
-    graph.generateRandomGraph(20,0.001,3)
+    graph.readGraph('graph.txt')
+    #graph.generateRandomGraph(20,0.001,3)
     generatePMFormula(graph, "formula.txt")
+    generateNEPMFormula(graph, "nepmformula.txt")
     PBEncoding("formula.txt", "pb.txt")
     print(graph.edges)
