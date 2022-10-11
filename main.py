393bc8dbc0fdba0121b76730fcd4add296096d7c
Zhang <zz59@nlogin2.nots.rice.edu>
2022-10-10 20:44:23 -0500

    Graph searching now works! But it's too slow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 import os
+import math
+import argparse
 class Graph:
     def __init__(self):
         self.n = 0
@@ -109,19 +111,18 @@ def generateGraphDiscoveryFormula(graph, PMFormulaPath, NEPMFormulaPath, PBXORPa
         for constraint in constraintList:
             f.write(constraint)
 
-def generatePMFormula(graph, formulaPath, varMap):
+def generatePMFormula(graph, formulaPath, varMap, state):
     for e in graph.edges:
         allocateVar(varMap, getPMEdgeString(e))
     for i in range(1, graph.n + 1):
         for j in range(1, graph.d + 1):
             allocateVar(varMap, getVCString(i, j))
-    #print(varMap)
 
     with open(formulaPath, 'w+') as f:
         # perfect matching edges
         f.write("c color constraints\n")
-        for e in graph.edges:
-            f.write("imply %d -> %d\n" % (varMap[getPMEdgeString(e)], varMap[getEdgeString(e)]))
+        #for e in graph.edges:
+        #    f.write("imply %d -> %d\n" % (varMap[getPMEdgeString(e)], varMap[getEdgeString(e)]))
         for e in graph.edges:
             f.write("im %d -> ( %d & %d ) \n" % (
                 varMap[getPMEdgeString(e)], varMap[getVCString(e[0], e[2])], varMap[getVCString(e[1], e[3])]))
@@ -144,21 +145,22 @@ def generatePMFormula(graph, formulaPath, varMap):
                 f.write(repr(varMap[getVCString(i, j)]) + " ")
             f.write('\n')
         # symmetric function for vertex coloring
-        # todo: NAE(vc(1,r),vc(2,r),...) for GHZ states
-        for j in range(1, graph.d + 1):
-            f.write("nae ")
+        if state == "GHZ":
+        #NAE(vc(1,r),vc(2,r),...) for GHZ states
+            for j in range(1, graph.d + 1):
+                f.write("nae ")
+                for i in range(1,graph.n+1):
+                    f.write("%d " % (varMap[getVCString(i,j)]))
+                f.write('\n')
+        elif state == "W": # todo: think of the negation of exact-one
+            f.write("eo ")
             for i in range(1,graph.n+1):
-                f.write("%d " % (varMap[getVCString(i,j)]))
-            f.write('\n')
-
-        #f.write("co ")
-        #for i in range(1, graph.n + 1):
-        #    f.write(repr(varMap[getVCString(i, 1)]) + " ")
-        #f.write('\n')
+                f.write("%d " % varMap[getVCString(i,1)])
+            f.write()
     return len(varMap)
 
 
-def generateNEPMFormula(graph, formulaPath, varMap):
+def generateNEPMFormula(graph, formulaPath, varMap, state):
     for i in range(1, graph.n + 1):
         allocateVar(varMap,getTutteVariableString(i))
     for i in range(1, graph.n + 1):
@@ -171,12 +173,11 @@ def generateNEPMFormula(graph, formulaPath, varMap):
     for i in range(1, graph.n + 1):
         for j in range(1, graph.d + 1):
             allocateVar(varMap, getVCString(i, j))
-    #print(varMap)
     with open(formulaPath, 'w+') as f:
         # exact-one for ad-hoc color of each vertex
         f.write("c exact-one for ad-hoc color of each vertex\n")
-        for e in graph.edges:
-            f.write("imply %d -> %d\n" % (varMap[getRestEdgeString(e)], varMap[getEdgeString(e)]))
+        #for e in graph.edges:
+        #    f.write("imply %d -> %d\n" % (varMap[getRestEdgeString(e)], varMap[getEdgeString(e)]))
         for i in range(1, graph.n + 1):
             f.write("eo ")
             for j in range(1, graph.d + 1):
@@ -208,12 +209,18 @@ def generateNEPMFormula(graph, formulaPath, varMap):
         f.write(">= 1 ;\n")
 
         # todo: AE(vc(1,r),vc(2,r),...) for GHZ states
-        for j in range(1, graph.d + 1):
-            f.write("ae ") # ae = all-equal
-            for i in range(1, graph.n + 1):
-                f.write("%d " % (varMap[getVCString(i, j)]))
+        if state == "GHZ":
+            for j in range(1, graph.d + 1):
+                f.write("ae ") # ae = all-equal
+                for i in range(1, graph.n + 1):
+                    f.write("%d " % (varMap[getVCString(i, j)]))
+                f.write('\n')
+            return len(varMap)
+        elif state == "W":
+            f.write("eo ")
+            for i in range(1,graph.n+1):
+                f.write("%d " % varMap[getVCString(i,1)])
             f.write('\n')
-        return len(varMap)
 
 def PBEncoding(formulaPath, varMap, constraintList):
     with open(formulaPath) as f:
@@ -263,23 +270,21 @@ def PBEncoding(formulaPath, varMap, constraintList):
                 constraintList.append("+1 x%s +1 x%s +1 x%s -1 x%s -1 x%s >= -1 ;\n" % (split[1],split[5],split[8],split[12],split[14]))
                 constraintList.append("-4 x%s -1 x%s -1 x%s +1 x%s +1 x%s >= -2 ;\n" % (split[1],split[5],split[8],split[12],split[14]))
             if split[0] == 'nae':
-                stringpos = ""
                 stringneg = ""
                 for k in range(1,len(split)):
-                    stringpos += ("+1 x%s " % split[k] )
-                    stringpos += ("-1 x%s " % split[k] )
-                constraintList.append(stringpos + ">= 1 ;\n") # nae can be encoded into two cardinality constraitns
+                    stringneg += ("-1 x%s " % split[k] )
                 constraintList.append(stringneg + ">= " + repr(-len(split)+2) + " ;\n")
             if split[0] == 'ae':
                 for k in range(1,len(split)-1):
                     constraintList.append("* xor x%s x%s 0\n" % (split[k],split[k+1]))
                         
                 
-def checkNEPM(graph,NEPMFormulaPath="nepmformula.txt", PBXORNEPMFormulaPath="pbxornepmformula.txt"): # check the nepm comdition for all legal states. if the extended graph with unassigned edges are true has no illegal PM, return true. otherwise return false.
+def checkNEPM(graph, state,learnedConstraints, NEPMFormulaPath="nepmformula.txt", PBXORNEPMFormulaPath="pbxornepmformula.txt"): # check the nepm comdition for all legal states. if the extended graph with unassigned edges are true has no illegal PM, return true. otherwise return false.
     varMap = {}
-    for e in graph.edges:
-        allocateVar(varMap,getEdgeString(e))
-    generateNEPMFormula(graph,NEPMFormulaPath,varMap)
+    if not checkLearnedConflicts(graph,learnedConstraints): return False
+#    for e in graph.edges:
+#        allocateVar(varMap,getEdgeString(e))
+    generateNEPMFormula(graph,NEPMFormulaPath,varMap,state)
     constraintList = ["* #variable= 1 #constraint= 1\n"]
     PBEncoding(NEPMFormulaPath, varMap, constraintList)
     constraintList[0] = "* #variable= %d #constraint= %d\n" % (len(varMap),len(constraintList) -1 )
@@ -289,9 +294,9 @@ def checkNEPM(graph,NEPMFormulaPath="nepmformula.txt", PBXORNEPMFormulaPath="pbx
     cmd = '../../linpb/build/linpb %s > nepmres.txt' % PBXORNEPMFormulaPath
     os.system(cmd)
     # todo: call libpb. if SAT, return False. otherwise return true
-    return readLinpbRes("nepmres.txt")
+    return readLinpbRes("nepmres.txt", learnedConstraints)
 
-def readLinpbRes(resFile):
+def readLinpbRes(resFile, learnedConstraints, problemType): # problemType = {'PM','NEPM'}
     with open(resFile,'r') as f:
         lines = f.readlines()
         for line in lines:
@@ -299,16 +304,20 @@ def readLinpbRes(resFile):
             if len(split) == 0: continue
             if split[0] == 's':
                 if split[1] == 'UNSATISFIABLE': return True
-                elif split[1] == 'SATISFIABLE': return False
-        print("none")
+                elif split[1] == 'SATISFIABLE': 
+                    #todo: retract the PM and add it to the forbidden list
+                    
+                    return False
+        print("error: none")
         exit(0)
         
 
-def checkPM(graph,PMFormulaPath="pmformula.txt", PBXORPMFormulaPath="pbxorpmformula.txt"): # if the extended graph with unassigned edges are false has no proof of non-existence of PM, return true. Otherwise return true
+def checkPM(graph, state, learnedConstraints, PMFormulaPath="pmformula.txt", PBXORPMFormulaPath="pbxorpmformula.txt"): # if the extended graph with unassigned edges are false has no proof of non-existence of PM, return true. Otherwise return true
     varMap = {}
-    for e in graph.edges:
-        allocateVar(varMap, getEdgeString(e))
-    generatePMFormula(graph, PMFormulaPath, varMap)
+    if not checkLearnedConflicts(graph,learnedConstraints): return False
+ #   for e in graph.edges:
+ #       allocateVar(varMap, getEdgeString(e))
+    generatePMFormula(graph, PMFormulaPath, varMap, state)
     constraintList = ["* #variable= 1 #constraint= 1\n"]
     PBEncoding(PMFormulaPath, varMap, constraintList)
     constraintList[0] = "* #variable= %d #constraint= %d\n" % (len(varMap), len(constraintList) - 1)
@@ -318,18 +327,17 @@ def checkPM(graph,PMFormulaPath="pmformula.txt", PBXORPMFormulaPath="pbxorpmform
     cmd = '../../linpb/build/linpb %s > pmres.txt' % PBXORPMFormulaPath
     os.system(cmd)
     # todo: call libpb. if SAT, return False. otherwise return true
-    return readLinpbRes("pmres.txt")    
+    return readLinpbRes("pmres.txt", learnedConstraints)    
 
 def variableSelection(varList):
     index = random.sample(range(len(varList)),1)[0]
     v = varList[index]
     varList.pop(index)
-    print('selected edge %d ' % v)
-    return v, False
+    #print('selected edge %d ' % v)
+    return v, True
 
 def stackToGraph(wholeGraph, n, d, edgeMap, variableStack, variablesLeft, extender): # if extender is true, all unassigned edges are assumed to present. otherwise all unassigned edges are absent
     graph = Graph()
-    print("whole graph has %d edges " % len(wholeGraph.edges))
     for s in variableStack:
         if s[1]:
             e = wholeGraph.edges[s[0]-1]
@@ -341,10 +349,8 @@ def stackToGraph(wholeGraph, n, d, edgeMap, variableStack, variablesLeft, extend
     graph.init(n,len(graph.edges),d)
     return graph
 
-def DPLLGraphSearch():
+def DPLLGraphSearch(n,d,state='GHZ'):
     # assign edges to variable set
-    n = 4
-    d = 2
     wholeGraph = Graph()
     wholeGraph.generateCompleteGraph(n,d)
     edgeMap = {}
@@ -352,13 +358,19 @@ def DPLLGraphSearch():
         allocateVar(edgeMap, getPMEdgeString(e))
     variableStack = []
     variablesLeft = list(range(1,len(edgeMap) + 1))
+    count = 0
+    prunedStr = ""
+    learnedConstraintsPM = []
+    learnedConstraintsNEPM = []
     while True:
-        print(variablesLeft)
-        print(variableStack)
-        nepmGraph = stackToGraph(wholeGraph, n,d,edgeMap,variableStack,variablesLeft,True)
+        count += 1
+        print("count = %d\n" % count)
+        #print(variablesLeft)
+        #print(variableStack)
+        nepmGraph = stackToGraph(wholeGraph, n, d, edgeMap, variableStack, variablesLeft, True)
         pmGraph = stackToGraph(wholeGraph, n,d,edgeMap,variableStack,variablesLeft,False)
-        nepmFlag = checkNEPM(nepmGraph)
-        pmFlag = checkPM(pmGraph)
+        nepmFlag = checkNEPM(nepmGraph, state, learnedConstraintsNEPM)
+        pmFlag = checkPM(pmGraph, state, learnedConstraintsPM)
         print("nepmFlag = %s pmFlag = %s" % (nepmFlag,pmFlag))
         if nepmFlag == True and pmFlag == True:
             if len(variablesLeft) == 0:
@@ -370,6 +382,8 @@ def DPLLGraphSearch():
                 v, value = variableSelection(variablesLeft)
                 variableStack.append([v, value, False])
         else:
+            prunedStr += "+%d " % len(variablesLeft)
+            print("pruned %s all edges %d" % (prunedStr, len(edgeMap)))
             while len(variableStack)>0 and variableStack[-1][2] == True:
                 v = variableStack.pop(-1)[0]
                 variablesLeft.append(v)
@@ -382,12 +396,13 @@ def DPLLGraphSearch():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    graph = Graph()
-    #graph.readGraph('graph.txt')
-    #graph.readGraph('bipartite.txt')
     #graph.generateRandomGraph(20,0.001,3)
     #graph.generateCompleteBipartiteGraph(20,22,1)
     #graph.generateCompleteGraph(6,3)
     #generateGraphDiscoveryFormula(graph,"pmformula.txt","nepmformula.txt","pbxor.txt")
-    #print(graph.edges)
-    DPLLGraphSearch()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('n',type=int,help='number of vertices')
+    parser.add_argument('d', type=int, help='number of edges')
+    parser.add_argument('state',type=str,help='GHZ,W')
+    args = parser.parse_args()
+    DPLLGraphSearch(args.n,args.d,args.state)
